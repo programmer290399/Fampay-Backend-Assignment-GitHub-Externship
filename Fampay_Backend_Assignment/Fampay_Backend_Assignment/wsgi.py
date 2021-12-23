12a7b0afe5814bb7ef936f6e2b7e8efa6c11486f
Saahil Ali <programmer290399@gmail.com>
2021-12-23 07:21:23 +0530

    Reformatted files

--- Fampay_Backend_Assignment/Fampay_Backend_Assignment/wsgi.py
+++ Fampay_Backend_Assignment/Fampay_Backend_Assignment/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fampay_Backend_Assignment.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Fampay_Backend_Assignment.settings")
 
 application = get_wsgi_application()
--- Fampay_Backend_Assignment/youtube_rest_api/apps.py
+++ Fampay_Backend_Assignment/youtube_rest_api/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class YoutubeRestApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'youtube_rest_api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "youtube_rest_api"
