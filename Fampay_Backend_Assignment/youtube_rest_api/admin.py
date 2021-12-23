16d256eff7fa33afb648fb80fe73a8e9e2f001fa
Saahil Ali <programmer290399@gmail.com>
2021-12-23 09:25:21 +0530

    Added model for videos

--- Fampay_Backend_Assignment/youtube_rest_api/admin.py
+++ Fampay_Backend_Assignment/youtube_rest_api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from youtube_rest_api.models import Video
+
+
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Video, VideoAdmin)
--- Fampay_Backend_Assignment/youtube_rest_api/models.py
+++ Fampay_Backend_Assignment/youtube_rest_api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+#  Video title, description, publishing datetime, thumbnails URLs
+class Video(models.Model):
+    video_title = models.CharField("Video Title", max_length=255)
+    description = models.TextField("Description")
+    published_on = models.DateTimeField("publishing datetime")
+    thumb_url = models.URLField(
+        "Thumbnail URL", max_length=255, help_text="The URL to the video thumbnail"
+    )
+    link = models.URLField(
+        "Video Link", max_length=255, help_text="The URL to the video page"
+    )
+
+    class Meta:
+        verbose_name = "video"
+        verbose_name_plural = "videos"
+        ordering = ["-published_on"]  # To sort in reverse chronological order
+
+    def __str__(self):
+        return self.video_title
